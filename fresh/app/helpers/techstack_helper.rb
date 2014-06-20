cb85e030b00792a38f6a989cc9c92d124f26f8c7
Randy Tolentino <makertolentino@gmail.com>
2014-06-20 13:06:37 -0500

    added tech stack

---
+++ fresh/app/helpers/techstack_helper.rb
@@ -0,0 +1,2 @@
+module TechstackHelper
+end
--- fresh/config/routes.rb
+++ fresh/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/about', to: 'about#index'
   get '/create', to: 'create#index'
   get '/contact', to: 'contact#index'
+  get '/techstack', to: 'techstack#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
