16b8c28229a29295a9056c442d42a036e13a2f2c
Randy Tolentino <makertolentino@gmail.com>
2014-07-02 10:39:55 -0500

    seperating domain logic from the DOM

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,48 @@
 //= require jquery_ujs
 //= require foundation
 
+(function () {
+
+	window.Fresh = {};
+
+	// Enitites
+	Fresh.Shirt = function (attrs) {
+		this.shirtColor = attrs.shirtColor;
+		this.symbol = attrs.symbol;
+		this.symbolColor = attrs.symbolColor;
+		this.phrase = attrs.phrase;
+		this.phraseColor = attrs.phraseColor;
+	};
+
+	// Database implementations
+	var LocalStorageDB = function () {
+		
+		this.saveShirt = function () {
+			// Write to localstorage
+		};
+
+		this.getShirts = function () {
+			// Get all shirts from localstorage
+		};
+	};
+
+	Fresh.db = new LocalStorageDB();
+
+	// Commands
+	Fresh.createShirt = function (attrs) {
+		// shirtColor, symbol, symbolColor, phrase, phraseColor
+		// Maybe do some validation here
+		var shirt = new Fresh.Shirt(attrs);
+		Fresh.db.saveShirt(shirt);
+		return {
+			success: true,
+			shirt: shirt
+		};
+	};
+
+})();
+
+
 $(function() {
   
   $(document).foundation();
@@ -23,9 +65,31 @@ $(function() {
 		$("#fresh").hide().fadeIn(2000);
 	});
 
-	$("#shirt_color").change(function(){
-		var shirt_color = $("#shirt_color").val();
-		$(".tshirt").attr("src", shirt_color);
+	var activeShirt = {
+		shirtColor: 'white',
+		symbol: '{  }',
+		symbolColor: 'f82576',
+		phrase: '!FALSE',
+		phraseColor: 'f82576'
+	};
+
+	var renderShirt = function (targetEl, shirt) {
+		$(".tshirt", targetEl).attr("src", "t-" + shirt.shirtColor + ".png");		
+		$(".design-a", targetEl).text(shirt.symbol);
+		$(".design-a", targetEl).css("color", '#' + shirt.symbolColor);
+		$(".design-b", targetEl).text(shirt.phrase);
+		$(".design-b", targetEl).css("color", '#' + shirt.phraseColor);
+	};
+
+	var $activeShirtEl = $('.creation .shirt');
+	renderShirt($activeShirtEl, activeShirt);
+
+	$("#shirt_color").change(function () {
+		var shirtColor = $("#shirt_color").val();
+		activeShirt.shirtColor = shirtColor;
+		
+		$(".tshirt", $activeShirtEl).attr("src", shirtColor);
+		// renderShirt($activeShirtEl, activeShirt);		
 	});
 	
 	$("#symbol").change(function(){
@@ -48,21 +112,25 @@ $(function() {
 		$(".design-b").css("color", color_phrase);
 		$('html, body').animate({scrollTop:0}, 'slow');
 	});
-	// 	console.log(code);
-	// 	e.preventDefault();
-
-	// $("#create_button").on('click', function(e){
-	// 	$("#creation").load("gallery #gallery");
-	// 	console.log('this designed was cloned');
-	// 	e.preventDefault();
-	// });
 
 });
 
-
 	var newIdCount = 1;
 	$("#create_button").on('click', function(e){
-		$(".creation").clone().attr('id', 'id'+ newIdCount++).appendTo("#gallery");
+		var result = Fresh.createShirt(activeShirt);
+		if (result.success) {
+			// Render new shirt with result.shirt
+			// Create new div for shirt
+			// Render it using renderShirt
+			// Add new shirt div to #gallery
+		}
+		else {
+			console.log("Shirt creation failed with error:", result.error);
+		}
+		// $(".creation").clone().appendTo("#gallery");
+		// $('.tshirt').attr('id', 'id'+ newIdCount++);
+		// $('.design-a').attr('id', 'id'+ newIdCount++);
+		// $('.design-b').attr('id', 'id'+ newIdCount++);
 		console.log('this designed was cloned');
 		e.preventDefault();
 	});
